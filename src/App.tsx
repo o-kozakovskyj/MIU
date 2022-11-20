0b981e33ef0dfba1a0c0fb82a7539961c27286f4
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-11-20 18:54:19 +0200

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,24 @@ import SideBar from "@components/SideBar";
 import { Box, Stack } from "@mui/system";
 import * as React from "react";
 import { useState } from "react";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { ThemeProvider, createTheme } from "@mui/system";
 import CssBaseline from "@mui/material/CssBaseline";
+import { blue } from "@mui/material/colors";
 
 const App = () => {
   const [mode, setMode] = useState("light");
+
   const darkTheme = createTheme({
+    spacing: 10,
     palette: {
       mode,
+      primary: {
+        main: blue[400],
+        light: "skyblue",
+      },
+      secondary: {
+        main: "#fff",
+      },
     },
   });
 
